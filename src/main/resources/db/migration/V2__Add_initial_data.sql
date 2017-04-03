39fb01e18cee323586f5eae7cc3c765b26193e99
erpero <eerhardo@gmail.com>
2017-04-03 17:36:57 +0300

    Example tables and data
    Added example tables and their data, modified exercises.

--- src/main/resources/db/migration/V2__Add_initial_data.sql
+++ src/main/resources/db/migration/V2__Add_initial_data.sql
@@ -4,69 +4,63 @@ insert into "user" (id, name, student_number, field) values (1, 'Juha Sipilä', 
 insert into "user" (id, name, student_number, field) values (2, 'Alexander Stubb', '00002', 'TKT');
 insert into "user" (id, name, student_number, field) values (3, 'Timo Soini', '00003', 'TKT');
 
+-- esimerkkitaulun opiskelijat
+INSERT INTO opiskelijat VALUES(1, 'Maija', 'TKO');
+INSERT INTO opiskelijat VALUES(2, 'Ville', 'TKO');
+INSERT INTO opiskelijat VALUES(3, 'Kalle', 'VT');
+INSERT INTO opiskelijat VALUES(4, 'Liisa', 'VT');
+-- esimerkkitaulun kurssit
+INSERT INTO kurssit VALUES(1, 'tkp', 'KI');
+INSERT INTO kurssit VALUES(2, 'oope', 'JL');
+INSERT INTO kurssit VALUES(3, 'tiko', 'MJ');
+-- esimerkkitaulun suoritukset
+INSERT INTO suoritukset VALUES(1, 1, 5);
+INSERT INTO suoritukset VALUES(1, 2, 4);
+INSERT INTO suoritukset VALUES(1, 3, 2);
+INSERT INTO suoritukset VALUES(2, 1, 5);
+INSERT INTO suoritukset VALUES(2, 2, 3);
+INSERT INTO suoritukset VALUES(2, 4, 3);
+INSERT INTO suoritukset VALUES(3, 1, 5);
+INSERT INTO suoritukset VALUES(3, 2, 4);
+
 -- Perusharjoitukset
 insert into exercise_list (id, description) values (0, 'Perusharjoitukset');
 
-insert into exercise (id, description, type, creator) values (0, 'Hae kaikki "exercise" taulun rivit', 'easy', 0);
-insert into example_answer (exercise, answer) values (0, 'SELECT * FROM exercise;');
+insert into exercise (id, description, type, creator) values (0, 'Valitse opettajien nimet', 'easy', 0);
+insert into example_answer (exercise, answer) values (0, 'SELECT opettaja FROM kurssit;');
 
-insert into exercise (id, description, type, creator) values (1, 'Hae kaikki "exercise_list" taulun rivit', 'easy', 0);
-insert into example_answer (exercise, answer) values (1, 'SELECT * FROM exercise_list;');
+insert into exercise (id, description, type, creator) values (1, 'Valitse opiskelijoiden nimet, joilla
+pääaineena on TKO.', 'easy', 0);
+insert into example_answer (exercise, answer) values (1, 'SELECT nimi FROM opiskelija
+WHERE p_aine = "TKO";');
+
+insert into exercise (id, description, type, creator) values (2, 'Mitkä ovat Villen suorittamien
+kurssien arvosanat?', 'easy', 0);
+insert into example_answer (exercise, answer) values (2, 'SELECT suoritukset.arvosana
+FROM opiskelijat, suoritukset
+WHERE opiskelijat.nro =
+suoritukset.op_nro AND
+opiskelijat.nimi = "Ville";');
+
 
 insert into exercise_list_exercise (exercise, exercise_list) values (0, 0);
 insert into exercise_list_exercise (exercise, exercise_list) values (1, 0);
+insert into exercise_list_exercise (exercise, exercise_list) values (2, 0);
 
 -- Haastavat harjoitukset
 insert into exercise_list (id, description) values (1, 'Haastavat harjoitukset');
 
-insert into exercise (id, description, type, creator) values (2, 'Hae käyttäjän nimi ja hänen suorittamiensa tehtävien määrä', 'medium', 0);
-insert into example_answer (exercise, answer) values (2,
-    'SELECT name FROM "user"'
-    'WHERE id IN'
-      '(SELECT "user" FROM session LEFT JOIN session_try ON session_try.session = session.id);'
-);
+insert into exercise (id, description, type, creator) values (3, 'Lisää opiskelija nimeltä Matti
+tietokantaan. Matin opiskelijanumero on
+1234 ja pääaine VT.', 'medium', 0);
+insert into example_answer (exercise, answer) values (3, 'INSERT INTO opiskelijat
+VALUES(1234, "Matti", "VT");');
 
-insert into exercise (id, description, type, creator) values (3, 'Hae käyttäjän nimi ja hänen suorittamiensa tehtävien määrä', 'hard', 0);
-insert into example_answer (exercise, answer) values (3,
-    'WITH started_exercises AS ('
-      'SELECT session, correct, COUNT(*) AS tries, exercise FROM session_try GROUP BY session, correct, exercise'
-    '), finished_exercises AS ('
-      'SELECT '
-        '"user", exercise_list, session, COUNT(*) '
-      'FROM session INNER JOIN started_exercises ON '
-          '(started_exercises.session = session.id AND'
-          '(started_exercises.tries = session.max_tries OR '
-          'started_exercises.correct))'
-      'GROUP BY "user", exercise_list, session'
-    ') select name, SUM(finished_exercises.count) from "user" LEFT JOIN finished_exercises on finished_exercises."user" = id GROUP BY name;'
-);
+insert into exercise (id, description, type, creator) values (4, 'Poista opiskelija, jonka numero on 1234.', 'medium', 0);
+insert into example_answer (exercise, answer) values (4, 'DELETE FROM opiskelijat WHERE
+nro = 1234;');
 
-insert into exercise (id, description, type, creator) values (4, 'Hae käyttäjien nimet ja heidän suorittamiensa tehtävälistojen nimet', 'hard', 0);
-insert into example_answer (exercise, answer) values (4,
-    'WITH started_exercises AS ('
-      'SELECT session, correct, COUNT(*) AS tries, exercise FROM session_try GROUP BY session, correct, exercise'
-    '), finished_exercises AS ('
-      'SELECT '
-        '"user", exercise_list, session, COUNT(*) '
-      'FROM session INNER JOIN started_exercises ON '
-          '(started_exercises.session = session.id AND '
-          '(started_exercises.tries = session.max_tries OR '
-          'started_exercises.correct)) '
-      'GROUP BY "user", exercise_list, session'
-    '), users_with_finished_exercise_lists AS ('
-      'SELECT '
-        '"user", description '
-      'FROM exercise_list '
-      'INNER JOIN finished_exercises '
-      'ON finished_exercises.count = exercise_list.exercise_amount'
-    ') SELECT '
-      'users_with_finished_exercise_lists.description, name '
-    'FROM "user" INNER JOIN '
-      'users_with_finished_exercise_lists '
-    'ON "user".id = users_with_finished_exercise_lists."user";'
-);
-
-insert into exercise_list_exercise (exercise, exercise_list) values (2, 1);
+insert into exercise_list_exercise (exercise, exercise_list) values (0, 1);
 insert into exercise_list_exercise (exercise, exercise_list) values (3, 1);
 insert into exercise_list_exercise (exercise, exercise_list) values (4, 1);
 
